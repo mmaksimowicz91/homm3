4e5f17f72d4af74946612015d7848d84ec27b3ff
Marcin Maksimowicz <grawmug@gmail.com>
2023-09-14 15:57:48 +0200

    WIP: Adventure module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,16 +17,6 @@ import { DwellingsComponent } from './modules/adventure/structures/dwellings/dwe
 import { MinesComponent } from './modules/adventure/structures/mines/mines.component';
 import { TreasuresComponent } from './modules/adventure/structures/treasures/treasures.component';
 import { TravelMiscComponent } from './modules/adventure/structures/treasures/travel-misc/travel-misc.component';
-import { CastleComponent } from './modules/adventure/structures/dwellings/castle/castle.component';
-import { RampartComponent } from './modules/adventure/structures/dwellings/rampart/rampart.component';
-import { InfernoComponent } from './modules/adventure/structures/dwellings/inferno/inferno.component';
-import { TowerComponent } from './modules/adventure/structures/dwellings/tower/tower.component';
-import { StrongholdComponent } from './modules/adventure/structures/dwellings/stronghold/stronghold.component';
-import { DungeonComponent } from './modules/adventure/structures/dwellings/dungeon/dungeon.component';
-import { FortressComponent } from './modules/adventure/structures/dwellings/fortress/fortress.component';
-import { ConfluxComponent } from './modules/adventure/structures/dwellings/conflux/conflux.component';
-import { NecropolisComponent } from './modules/adventure/structures/dwellings/necropolis/necropolis.component';
-import { NeutralComponent } from './modules/adventure/structures/dwellings/neutral/neutral.component';
 
 @NgModule({
   declarations: [
@@ -39,16 +29,6 @@ import { NeutralComponent } from './modules/adventure/structures/dwellings/neutr
     MinesComponent,
     TreasuresComponent,
     TravelMiscComponent,
-    CastleComponent,
-    RampartComponent,
-    InfernoComponent,
-    TowerComponent,
-    StrongholdComponent,
-    DungeonComponent,
-    FortressComponent,
-    ConfluxComponent,
-    NecropolisComponent,
-    NeutralComponent,
   ],
   imports: [
     // RouterModule.forRoot(routes),
